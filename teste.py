2736968e1b3694086793be275629125344505010
Carlos Eduardo Ribeiro <vitorhugopiske.trapp@philips.com>
2025-05-05 15:34:55 -0300

    [SO-0000] feat(XPTO) added a type for the input

--- teste.py
+++ teste.py
@@ -1,5 +1,5 @@
-numero1 = input("Digite o primeiro número: ")
-numero2 = input("Digite o segundo número: ")
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
 
 soma = numero1 + numero2
 
